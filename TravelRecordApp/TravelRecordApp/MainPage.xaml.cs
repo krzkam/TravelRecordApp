f7589f0b4e66d2608890cb57ca59de83f237728b
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-10 12:13:38 +0100

    030 The Login Logic

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -16,7 +16,17 @@ namespace TravelRecordApp
 
         private void LoginButton_Clicked(object sender, EventArgs e)
         {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+
+            }
         }
     }
 }

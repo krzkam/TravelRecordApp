f00f02a9f88d0c3edffb2958062d546ba58c511a
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-16 12:33:21 +0100

    057 The IDisposable interface and Using statements

--- TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
@@ -22,10 +22,12 @@ namespace TravelRecordApp
         {
             base.OnAppearing();
 
-            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
-            conn.CreateTable<Post>();
-            var posts = conn.Table<Post>().ToList();
-            conn.Close();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                var posts = conn.Table<Post>().ToList();
+            }
+            //conn.Close();
         }
     }
 }
--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -25,19 +25,25 @@ namespace TravelRecordApp
                 Experience = experienceEntry.Text 
             };
 
-            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
-            conn.CreateTable<Post>();
-            int rows = conn.Insert(post);
-            conn.Close();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
 
-            if (rows>0)
-            {
-                DisplayAlert("Success","Experience succesfully inserted","Ok");
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience succesfully inserted", "Ok");
+                }
+                else
+                {
+                    DisplayAlert("Failed", "Experience failed to be inserted", "Ok");
+                }
             }
-            else
-            {
-                DisplayAlert("Failed", "Experience failed to be inserted", "Ok");
-            }
+
+
+            //conn.Close();
+
+
         }
     }
 }

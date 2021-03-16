8a6ee72c542c5bfbce488e82554f9cb971da7b20
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-16 12:02:07 +0100

    055 Inserting into the Database

--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,27 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text 
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows>0)
+            {
+                DisplayAlert("Success","Experience succesfully inserted","Ok");
+            }
+            else
+            {
+                DisplayAlert("Failed", "Experience failed to be inserted", "Ok");
+            }
+        }
+    }
 }

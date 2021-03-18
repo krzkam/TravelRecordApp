c36049422bc2645176ec27d60cfdfaba106c465e
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-18 13:46:27 +0100

    066 Creating URL for REST Request

---
+++ TravelRecordApp/TravelRecordApp/Model/Venue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitude,double longitude)
+        {
+            return string.Format(Constans.VENUE_SEARCH, latitude, longitude,Constans.CLIENT_ID,Constans.CLIENT_ID,DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}

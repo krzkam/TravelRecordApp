3e56107b0351423b2fc3e421bc3df0b4d9abe4fe
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-22 12:51:10 +0100

    073 Displaying pins on a map

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,13 +1,15 @@
 ﻿using Plugin.Geolocator;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
@@ -36,7 +38,70 @@ namespace TravelRecordApp
             var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
             var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
             locationsMap.MoveToRegion(span);
+
+            var pinPosition = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+
+            var pin = new Pin()
+            {
+                Type = PinType.SavedPin,
+                Position = pinPosition,
+                Label = "Exeption Label",
+                Address = "Exeption Address",
+
+            };
+
+            locationsMap.Pins.Add(pin);
+
+            //using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            //{
+            //    conn.CreateTable<Post>();
+            //    var posts = conn.Table<Post>().ToList();
+
+            //    DisplayInMap(posts);
+            //}
         }
+
+        protected override async void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            var locator = CrossGeolocator.Current;
+            locator.PositionChanged -= Locator_PositionChanged;
+
+            await locator.StopListeningAsync();
+
+        }
+
+        private void DisplayInMap(List<Post> posts)
+        {
+            foreach (var post in posts)
+            {
+                try
+                {
+                    var position = new Xamarin.Forms.Maps.Position(post.Latitude, post.Longitude);
+
+                    var pin = new Pin()
+                    {
+                        Type = PinType.SavedPin,
+                        Position = position,
+                        Label = post.VenueName,
+                        Address = post.Address,
+
+                    };
+
+                    locationsMap.Pins.Add(pin);
+                }
+                catch (NullReferenceException nre)
+                {
+     
+                }
+                catch (Exception e)
+                {
+        
+                }
+            }
+        }
+ 
 
         private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
         {

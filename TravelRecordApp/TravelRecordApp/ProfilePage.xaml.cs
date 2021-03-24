9176cb2ca288f30cd3f4034bd83c273cf9d63bb4
Krzysztof Kamiński <krzkaminski@wp.pl>
2021-03-24 15:19:57 +0100

    081 ListViews and Dictionaries

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -24,26 +24,35 @@ namespace TravelRecordApp
 
             using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
             {
-                var postTable = conn.Table<Post>();
+                var postTable = conn.Table<Post>().ToList();
 
                 var categories = (from p in postTable
                                   orderby p.CategoryId
                                   select p.CategoryName).Distinct().ToList();
 
                 Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
-
                 foreach (var category in categories)
                 {
                     var count = (from post in postTable
                                  where post.CategoryName == category
                                  select post).ToList().Count;
 
-                    categoriesCount.Add(category, count);
+                    var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    try
+                    {
+                        categoriesCount.Add(category, count2);
+                    }
+                    catch (Exception)
+                    {
+
+                        //throw;
+                    }
                 }
 
+                categoriesListView.ItemsSource = categoriesCount;
 
-
-                postCountLabel.Text = postTable.Count().ToString();
+                postCountLabel.Text = postTable.Count.ToString();
             }
         }
     }
